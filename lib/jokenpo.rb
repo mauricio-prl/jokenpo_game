de984c338849244c1a6c36e697bc33cd501c09bc
Mauricio Lima <mauricio@collateralxp.com>
2024-03-24 16:37:43 -0300

    improve available_moves

--- lib/jokenpo.rb
+++ lib/jokenpo.rb
@@ -1,10 +1,9 @@
 class Jokenpo
-  AVAILABLE_MOVES = %w[rock paper scissors].freeze
   WINNING_MOVES = { 'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper' }.freeze
 
   def self.play(...) = new(...).play
 
-  def initialize(input, machine = AVAILABLE_MOVES.sample)
+  def initialize(input, machine = available_moves.sample)
     @input = input
     @machine = machine
   end
@@ -21,8 +20,10 @@ class Jokenpo
 
   attr_reader :input, :machine
 
+  def available_moves = WINNING_MOVES.keys
+
   def validate!
-    return if [input, machine].all? { |move| AVAILABLE_MOVES.include?(move) }
+    return if [input, machine].all? { |move| available_moves.include?(move) }
 
     raise ArgumentError, "Invalid move: #{input} or #{machine}"
   end
